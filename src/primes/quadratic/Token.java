1a008d3e1c07fdf67f9785f5351c053f5170f99b
Marco Pedicini <marco@iac.cnr.it>
2018-05-03 15:08:05 +0200

    piccole correzioni

--- src/primes/quadratic/Token.java
+++ src/primes/quadratic/Token.java
@@ -7,63 +7,37 @@
 package primes.quadratic ;
 
 import java.math.BigInteger ;
-import primes.erathostenes.Sieve ;
+//import primes.erathostenes.Sieve ;
 
 class Token extends primes.erathostenes.Token {
-//private BigInteger num ;
-private BigInteger safenum ;
-//private Item row ;
-private boolean primality ;
+	private BigInteger safenum ;
+	private boolean primality ;
 
 	static final BigInteger TWO = new BigInteger("2");
 	
 	//costruttori
-	 Token() {
-		 System.out.println("new quadratic Token");
-		 
-		 
+	Token() {
+		System.out.println("new quadratic Token");
 		this.Set(BigInteger.ZERO) ;
-		
 	}
 
 	// setters
-/*	void Set(BigInteger n) {
-		
-		this.num = n ;
-		
-		
-	}
-*/
-	
 	public void Set(BigInteger n) {
 		super.Set(n) ;
 		this.safenum = n;
-
-		//this.row = null ;
 		this.primality = true ;
 	}
 	
-	
-	// getters
-/*	BigInteger value() {
-		
-		return this.num;
-	}
-*/
 	public void SetPrimality(boolean b) {
-		
 		this.primality = b;
 	}
 	
 	public BigInteger value2() {
 		System.out.println("in quadratic Token "+this.safenum);
-		
 		return this.safenum ;
-		
 	}
 	
 	boolean primality() {
-		
 		return this.primality;
 	}
 
